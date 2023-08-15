a5b98c8b7db7391baf540f87c086e6336b93084f
fletcher burton <burton.fletcher@aol.com>
2023-08-14 23:49:04 -0500

    done

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-// Before you begin coding, be sure that you:
-// a. Cleaned up the coffee_data.js file
-// b. Connect the coffee_data.js file to this index.js file.
+const  coffeeMenu  = require("./coffee_data_starter");
 
-// Prompt 2:
-// Print an array of all the drinks.
+//Question 2.
+//Step 1. Print an array of all drinks on the menu.
+console.log('ALL THE DRINKS')
+coffeeMenu.map((list, index) => console.log(list, index));
+// let drinkNames = coffeeMenu.map((element) => console.log(element.name));
+// console.log(drinkNames)
 
-// Prompt 3
-// Print an array of drinks that cost 5 and under.
 
-// Prompt 4
-// Print an array of drinks that are priced at an even number.
+//Question 3.
+//Step 1. Create a function that filters out any drink that cost more than $5.
+////Step 2. Print a new array with the found data.
 
-// Prompt 5
-// Print the total if you were to order one of every drink.
+const drinksUnderFive = coffeeMenu.filter((drink) => 
+drink.price <= 5);
+    if(drinksUnderFive.price != 6){
+console.log('DRINKS THAT ARE 5 BUCKS OR LESS')
+    }else{
+        console.log('error')
+    }
 
-// Prompt 6
-// Print an array with all the drinks that are seasonal.
+console.log(drinksUnderFive);
 
-// Prompt 7
-// Print all the seasonal drinks with the words "with imported beans" after the item name.
-// For example: "affogato with imported beans"
+//Question 4.
+// Step 1. Create a function that stores any drinks that cost an even number to a new array.
+
+const drinksEven = coffeeMenu.filter((drink) => 
+    drink.price % 2 === 0);
+    if(drinksEven.price % 2 !== 0){
+console.log('DRINKS THAT ARE EVEN')
+    }else{
+        console.log('error')
+    }
+
+console.log(drinksEven);
+
+//Question 5.
+//Step 1. Create a function that takes the price of each drink.
+////Step 2. Add the prices together.
+/////Step 3. Print a new array.
+
+const drinksTotal = coffeeMenu.reduce((p, c) => {
+  return p + c.price
+}, 0);
+
+if(drinksTotal === 64){
+console.log("ALL THE DRINKS ADDED TOGETHER")
+}
+
+console.log(drinksTotal);
+
+// Question 6.
+//Step 1. Create a function that prints any drinks "if" drinks are seasonal.
+
+const drinksSeasonal = coffeeMenu.filter((drink) => {
+  if (drink.seasonal === true) {
+    return drink.seasonal;
+  }
+});
+
+if(drinksSeasonal !== true){
+console.log('SEASONAL DRINKS')
+}
+
+console.log(drinksSeasonal);
+
+// Question 6.
+//Step 1. Create a function that filters out only seasonal drinks.
+////Step 2. If the drink is seasonal, add 'With imported beans' after then name
+
+let importedBeans = coffeeMenu.map((drink) => {
+  if (drink.seasonal === true && drink.name !== null) {
+    return `${drink.name} with imported beens`;
+  }
+});
+
+console.log(importedBeans);
